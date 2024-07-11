a33104795b9df20b55c322a7b5028c49c19ef4bb
bbpandc <haeminsia@gmail.com>
2024-07-11 17:00:07 +0900

    search_dvd, search_producer

---
+++ src/main/webapp/static/search_producer.js
@@ -0,0 +1,5 @@
+function handleSearchClick() {
+			const searchInput = document.querySelector(".search-input");
+			// 해당 주소로 이동
+			location.href = `http://localhost:8080/dvd/producer/search?searchText=${searchInput.value}`;
+		}
